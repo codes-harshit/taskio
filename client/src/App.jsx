04419b6338037b2b2201f5caefd7efe663931f56
codes-harshit <programming.harshit44@gmail.com>
2025-04-07 18:35:42 +0530

    backend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="text-center text-red-500 text-2xl">TrackTodo</div>
+    </>
+  );
+}
+
+export default App;
